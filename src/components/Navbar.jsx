448cc671f4ec6ffeb7b1d6c31e8ee20d0ddaf558
Julius Peter Ntale <juliusntale30@gmail.com>
2024-12-28 22:45:49 +0300

    Changes to be committed: 	new file:   index.html 	new file:   node_modules/.bin/acorn 	new file:   node_modules/.bin/acorn.cmd 	new file:   node_modules/.bin/acorn.ps1 	new file:   node_modules/.bin/autoprefixer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="p-4 bg-gray-100 shadow-lg">
+            <div className="flex justify-between items-center">
+                <span className="text-xl font-bold text-gray-800">Booking App</span>
+                <div className="flex space-x-4">
+                    <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white hover:bg-white hover:text-black transition-colors duration-300">
+                        Register
+                    </button>
+                    <button className="p-4 rounded-lg ring-1 ring-black bg-white text-black hover:bg-black hover:text-white transition-colors duration-300">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
